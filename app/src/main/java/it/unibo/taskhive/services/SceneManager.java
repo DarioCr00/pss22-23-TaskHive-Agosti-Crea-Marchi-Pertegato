a61c034b1b2fd165a16ecc4b94d9cfda8de42ca9
enricomarchi8 <enrico.marchi8@studio.unibo.it>
2025-09-20 17:12:29 +0200

    feat(view): added ProjectView and style

---
+++ app/src/main/java/it/unibo/taskhive/services/SceneManager.java
@@ -0,0 +1,33 @@
+package it.unibo.taskhive.services;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneManager {
+    private static Stage primaryStage;
+
+    public static void setStage(Stage stage) {
+        primaryStage = stage;
+        primaryStage.setTitle("TaskHive");
+    }
+
+    public static void switchScene(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(SceneManager.class.getClassLoader().getResource(fxmlPath));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
+
+            scene.getStylesheets().add(SceneManager.class.getClassLoader().getResource("css/styles.css").toExternalForm());
+    
+            primaryStage.setScene(scene);
+            primaryStage.setMaximized(true);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
